24b8a9f1498727be1d8c5f3e9a7294c1d12eb033
Haron Paulson <haron.paulson@anitechgroup.com>
2023-04-26 10:57:12 +0530

    deploy to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Details from './Details';
+import Navigation from './Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App mt-2 bg-gray-100">
+      <h1 className=' bg-black'>.</h1>
+      <Details />
+      <Navigation />
     </div>
   );
 }
---
+++ src/Details.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './index.css';
+
+function Details () {
+    return(
+        <div className=" w-4/6">
+            <img src= "anitechlogo.png" alt="companylogo" className=" w-40 mb-2 ml-52"/>
+            <h1 className=" font-semibold mb-2 text-2xl pl- ">Cost Estimate for audiometric assessment</h1>
+        </div>
+    )
+}
+
+export default Details;
